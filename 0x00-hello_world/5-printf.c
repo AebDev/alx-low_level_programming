80360ca35b88697f3ae0c85293e90b2653e50fe1
aebdev <mohcine.aeb@gmail.com>
2023-03-16 12:58:49 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * start main function
+ * use printf
+ * return 0
+ * end main function
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
